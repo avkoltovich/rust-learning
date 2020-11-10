581a5d1d90833ec9c46ab84be094cee36e71c96b
Aleksandr Koltovich <avkoltovich@gmail.com>
2020-11-10 20:53:31 +0300

    feat: добавляет примеры с match

---
+++ match_examples/src/main.rs
@@ -0,0 +1,34 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+#[derive(Debug)]
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn main() {
+    let value = value_in_cents(Coin::Quarter(UsState::Alabama));
+
+    println!("Значение в центах: {}", value)
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}

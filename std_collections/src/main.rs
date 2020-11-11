ec1f48f24e57e4018eb874c81cf9c216ea40b803
Aleksandr Koltovich <avkoltovich@gmail.com>
2020-11-11 21:26:04 +0300

    feat: добавляет примеры коллекций стандартной библиотеки

--- std_collections/src/main.rs
+++ std_collections/src/main.rs
@@ -1,5 +1,7 @@
+use std::collections::HashMap;
+
 fn main() {
-    let v = vec![1, 2, 3, 4, 5];
+    let mut v = vec![1, 2, 3, 4, 5];
 
     let third: &i32 = &v[2];
     println!("Третий элемент вектора: {}", third);
@@ -8,4 +10,23 @@ fn main() {
         Some(third) => println!("Третий элемент вектора: {}", third),
         None => println!("Нет значения"),
     }
+
+    v.push(56);
+
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
 }

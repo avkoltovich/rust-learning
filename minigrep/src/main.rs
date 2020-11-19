4ad2525c7d13878f6a9345fd29d0e61483a666a0
Aleksandr Koltovich <avkoltovich@gmail.com>
2020-11-19 22:21:24 +0300

    feat: добавляет возможность чтения файлов

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,17 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+
+    let query = &args[ 1 ];
+    let file_name = &args[ 2 ];
+
+    println!("Ищем {}", query);
+    println!("В файле {}", file_name);
+
+    let contents = fs::read_to_string(file_name)
+        .expect("Произошла ошибка во время чтения файла");
+
+        println!("С текстом:\n{}", contents);
 }

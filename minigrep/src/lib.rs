92efdbee8751ce128d9b9d1107a43e23355a748d
Aleksandr Koltovich <avkoltovich@gmail.com>
2020-11-20 22:18:21 +0300

    refactor: разделили код на библиотечный крейт

---
+++ minigrep/src/lib.rs
@@ -0,0 +1,28 @@
+use std::fs;
+use std::error::Error;
+
+pub struct Config {
+    pub query: String,
+    pub file_name: String
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("недостаточно аргументов")
+        }
+
+        let query = args[ 1 ].clone();
+        let file_name = args[ 2 ].clone();
+
+        Ok(Config { query, file_name })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_name)?;
+
+        println!("С текстом:\n{}", contents);
+
+        Ok(())
+}
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -14,36 +14,9 @@ fn main() {
     println!("Ищем {}", config.query);
     println!("В файле {}", config.file_name);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Ошибка приложения: {}", e);
 
         process::exit(1);
     }
 }
-
-struct Config {
-    query: String,
-    file_name: String
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("недостаточно аргументов")
-        }
-
-        let query = args[ 1 ].clone();
-        let file_name = args[ 2 ].clone();
-
-        Ok(Config { query, file_name })
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_name)?;
-
-        println!("С текстом:\n{}", contents);
-
-        Ok(())
-}
-

14f737c2812d22c841d735fae8edd8e6bc07d183
Aleksandr Koltovich <avkoltovich@gmail.com>
2020-11-23 21:20:01 +0300

    feat: реализована функциональность поиска подстроки в файле

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -7,15 +7,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Проблема со чтением аргументов: {}", err);
+        eprintln!("Проблема со чтением аргументов: {}", err);
         process::exit(1);
     });
 
-    println!("Ищем {}", config.query);
-    println!("В файле {}", config.file_name);
-
     if let Err(e) = minigrep::run(config) {
-        println!("Ошибка приложения: {}", e);
+        eprintln!("Ошибка приложения: {}", e);
 
         process::exit(1);
     }
--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,7 +1,29 @@
 fn main() {
-    let a = [1, 2, 3, 4, 5];
+    let arr_a = &[-3, 4, 2];
+    let arr_b = &[1, 2, 3];
 
-    let first = a[0];
-    let second = a[1];
-    
+    let result = solve("CODe");
+
+    println!("{:?}", result)
+}
+
+fn solve(s: &str) -> String {
+    let mut upper_count = 0;
+    let mut lower_count = 0;
+
+    let string = s.to_string();
+
+    for symbol in string.chars() {
+        if symbol.is_ascii_lowercase() {
+            lower_count += 1;
+        } else {
+            upper_count += 1;
+        }
+    }
+
+    if lower_count >= upper_count {
+        return string.to_lowercase();
+    } else {
+        return string.to_uppercase();
+    }
 }
